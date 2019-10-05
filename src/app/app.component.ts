8b3361bed442ed7f45e36d418bc2546b14538397
Suyash <suyash.champ1@gmail.com>
2019-10-06 01:52:45 +0530

    added average field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatInput} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,8 @@ export class AppComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
-  semestersCgpas: {};
+  semestersSoFar: number;
   semesterList: any[];
-
   result: number;
   avg: number;
 
@@ -21,6 +21,10 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initialiseForm();
+  }
+
+  initialiseForm() {
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: [
         '',
@@ -33,7 +37,7 @@ export class AppComponent implements OnInit {
       ]
     });
     this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+      secondCtrl: []
     });
     this.thirdFormGroup = this._formBuilder.group({
       expectedCgpa: [
@@ -50,21 +54,36 @@ export class AppComponent implements OnInit {
   }
 
   gotSemesters() {
-    const semestersSoFar = this.firstFormGroup.value.firstCtrl;
-
+    this.semestersSoFar = this.firstFormGroup.value.firstCtrl;
+    // const semestersSoFar = this.firstFormGroup.value.firstCtrl;
     this.thirdFormGroup.controls.expectedSemester.setValidators([
-      Validators.min(semestersSoFar + 1),
+      Validators.min(this.semestersSoFar + 1),
       Validators.max(10),
       Validators.required,
       Validators.pattern('^[0-9]*$')
     ]);
     this.thirdFormGroup.controls.expectedSemester.updateValueAndValidity();
 
-    this.semestersCgpas = {};
-    for (let i = 1; i <= semestersSoFar; i++) {
-      this.semestersCgpas = {
-        ...this.semestersCgpas,
+
+    let semestersCgpasGroup = {};
+    for (let i = 1; i <= this.semestersSoFar; i++) {
+      semestersCgpasGroup = {
+        ...semestersCgpasGroup,
         [i]: [
+          '',
+          [
+            Validators.required,
+            Validators.min(1),
+            Validators.max(10),
+            Validators.pattern('[0-9](.[0-9][0-9]?)?')
+          ],
+        ]
+      };
+    }
+    let secondForm = {semesters: this._formBuilder.group(semestersCgpasGroup)};
+    if (this.semestersSoFar > 1) {
+      const avg = {
+        ['avg']: [
           '',
           [
             Validators.required,
@@ -74,27 +93,64 @@ export class AppComponent implements OnInit {
           ]
         ]
       };
+      secondForm = {
+        ...secondForm,
+        ...avg
+      };
     }
-    this.secondFormGroup = this._formBuilder.group(this.semestersCgpas);
-    this.semesterList = Object.keys(this.semestersCgpas);
+    // this.secondFormGroup = this._formBuilder.group(this.semestersCgpas);
+    this.secondFormGroup = this._formBuilder.group(secondForm);
+    this.semesterList = Object.keys(semestersCgpasGroup);
+    // console.log('second form group creation', this.secondFormGroup, 'semester list', this.semesterList);
+    this.onChanges();
+  }
+
+  onChanges() {
+    if (this.semestersSoFar > 1) {
+      this.secondFormGroup.get('avg').valueChanges.subscribe(avg => {
+        if (avg) {
+          this.secondFormGroup.get('semesters').disable({emitEvent: false});
+        } else {
+          this.secondFormGroup.get('semesters').enable({emitEvent: false});
+        }
+      });
+      this.secondFormGroup.get('semesters').valueChanges.subscribe(val => {
+        let disableAvg = false;
+        Object.keys(val).forEach(sem => {
+          disableAvg = disableAvg || !!val[sem];
+        });
+        if (disableAvg) {
+          this.secondFormGroup.get('avg').disable({emitEvent: false});
+        } else {
+          this.secondFormGroup.get('avg').enable({emitEvent: false});
+        }
+      });
+    }
   }
 
   gotCgpas() {
+    // console.log('second form', this.secondFormGroup);
   }
 
   gotExpectedData() {
-    const semestersSoFar = this.firstFormGroup.value.firstCtrl;
-    const semestersCgpas = this.secondFormGroup.value;
+    let semestersCgpas;
     const expectedGPA = this.thirdFormGroup.value.expectedCgpa;
     const expectedSem = this.thirdFormGroup.value.expectedSemester;
-
     let sum = 0;
-    Object.keys(semestersCgpas).forEach((sem: any) => {
-      sum += parseFloat(semestersCgpas[sem]);
-    });
-    this.avg = sum / semestersSoFar;
+    if (this.semestersSoFar > 1 && this.secondFormGroup.value.avg) {
+      this.avg = this.secondFormGroup.value.avg;
+      sum = this.avg * this.semestersSoFar;
+    } else {
+      semestersCgpas = this.secondFormGroup.value.semesters;
+      Object.keys(semestersCgpas).forEach((sem: any) => {
+        if (sem !== 'avg') {
+          sum += parseFloat(semestersCgpas[sem]);
+        }
+      });
+      this.avg = sum / this.semestersSoFar;
+    }
     this.result =
-      (expectedGPA * expectedSem - sum) / (expectedSem - semestersSoFar);
+      (expectedGPA * expectedSem - sum) / (expectedSem - this.semestersSoFar);
     this.result = parseFloat(this.result.toFixed(2));
   }
 }
